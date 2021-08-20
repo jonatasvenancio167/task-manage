eeaf1fbbda47aa2c72d65125682847ea05d8ed40
jonatasvenancio167 <jvenancio167@hotmail.com>
2021-08-20 12:39:17 -0300

    configuring rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryBot.build(:user) }
-
-  it { expect(@user).to respond_to(:email) }
-  it { expect(@user).to respond_to(:name) }
-  it { expect(@user).to respond_to(:password) }
-  it { expect(@user).to respond_to(:password_confirmation) }
-  it { expect(@user).to be_valid }
+  subject { build(:user) }
+  
+  it { expect(subject).to respond_to(:email) }
+  it { is_expected.to be_valid }
 end
